3382afdaad1e9259176df39a14120a84fb44a258
Mikael <mbotassi@elfsm.com.br>
2026-07-27 09:17:07 -0300

    feat: implementa o service para o todos

--- docs/solutions/angular/first-question/src/app/features/todo-list/services/todo.service.ts
+++ docs/solutions/angular/first-question/src/app/features/todo-list/services/todo.service.ts
@@ -1,35 +1,19 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
 import { Todo } from '../models/todo.model';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class TodosApiService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'https://api.exemplo.com/todos';
 
-@Injectable({providedIn: 'root'})
-export class TodoService {
-
-  getTodos(): Observable<Todo[]> {
-    const todos: Todo[] = [
-      {
-        id: 1,
-        title: 'Estudar Angular',
-        completed: false
-      },
-
-      {
-        id: 2,
-        title: 'Implementar NgRx',
-        completed: true
-      },
-
-      {
-        id: 3,
-        title: 'Criar testes',
-        completed: false
-      }
-
-    ];
-    return of(todos).pipe(delay(1000));
+  getTodos(): Observable<ReadonlyArray<Todo>> {
+    return this.http.get<ReadonlyArray<Todo>>(
+      this.apiUrl
+    );
   }
-
 }
